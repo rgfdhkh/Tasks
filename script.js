fb56900e06d2a6a2a4f865ad3c5d3f069787cedf
简 <1821743054@qq.com>
2024-11-19 00:23:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showAlertDev(event) {
+    event.preventDefault();
+    alert("网页还在开发中，敬请期待喔！😘😘😘");
+}
+
+function showAlertEmpty(event) {
+    event.preventDefault();
+    alert("不要点啦，这啥也木有哇！😅😅😅");
+}
